7091adc1f0dc9a389df76995ae590c938c787fce
Sarah Huguley <SarahHuguley92@gmail.com>
2023-11-13 15:43:32 -0500

    Initial Commit

---
+++ HProfileChp1/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  HProfileChp1
+//
+//  Created by Sarah Huguley on 11/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Blast From The Past!")
+                .padding()
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                
+            Text("Chapter 1")
+            Text("By Sarah Huguley")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HProfileChp1/HProfileChp1App.swift
@@ -0,0 +1,17 @@
+//
+//  HProfileChp1App.swift
+//  HProfileChp1
+//
+//  Created by Sarah Huguley on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct HProfileChp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
