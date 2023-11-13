db5e9d82a3cba80612dc753527a438380461b967
Arika Handy <ahandy23@msu.ideserve.net>
2023-11-13 16:09:29 -0500

    Test run

---
+++ HProfileChp1/HProfileChp 4.swift
@@ -0,0 +1,18 @@
+//
+//  HProfileChp 4.swift
+//  HProfileChp1
+//
+//  Created by Arika Nicole Handy on 11/13/23.
+//
+
+import SwiftUI
+
+struct HProfileChp_4: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HProfileChp_4()
+}
