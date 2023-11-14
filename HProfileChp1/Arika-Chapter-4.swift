cf965a2bac7a4d411589c34f8e55b7a9909278c5
Arika Handy <ahandy23@msu.ideserve.net>
2023-11-14 13:36:17 -0500

    Added Arika's View

---
+++ HProfileChp1/Arika-Chapter-4.swift
@@ -0,0 +1,18 @@
+//
+//  Arika-Chapter-4.swift
+//  HProfileChp1
+//
+//  Created by Arika Nicole Handy on 11/14/23.
+//
+
+import SwiftUI
+
+struct Arika_Chapter_4: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Arika_Chapter_4()
+}
