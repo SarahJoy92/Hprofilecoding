1131ba03f1b20a7814bb38a840410df2fa9fa992
Sarah Huguley <SarahHuguley92@gmail.com>
2023-11-15 16:52:10 -0500

    added page 2

---
+++ HProfileChp1/Chp1Page2.swift
@@ -0,0 +1,18 @@
+//
+//  Chp1Page2.swift
+//  HProfileChp1
+//
+//  Created by Sarah Huguley on 11/15/23.
+//
+
+import SwiftUI
+
+struct Chp1Page2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Chp1Page2()
+}
