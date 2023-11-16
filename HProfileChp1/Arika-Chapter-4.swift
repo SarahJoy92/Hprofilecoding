f9ff710bc34ba9c4da595599c0b9b9db4b2c9389
Arika Handy <ahandy23@msu.ideserve.net>
2023-11-16 15:17:14 -0500

    Buttons added

--- HProfileChp1/Arika-Chapter-4.swift
+++ HProfileChp1/Arika-Chapter-4.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct Arika_Chapter_4: View {
-    @State private var drinks = ["Patron", "DonJulio", "WhiteZenfandel", "Mojito"]
+    @State private var drinks = ["Patron", "DonJulio", "Green Tea Shots", "Jack Daniels"]
     @State private var drink = "patron"
     @State private var friends = ["Amanda","Tammy","Olivia","Jasmine"]
     @State private var friend = "Amanda"
@@ -32,38 +32,66 @@ struct Arika_Chapter_4: View {
                     Spacer()
                     
                     VStack {
-                        Text("   Amanda stared at her reflection in the mirror feeling down from her day. When she wins a case she has a slice of Caramel Cake but if she loses she has a glass of wine. She wondered if she should go out or stay home. Just then she received a text form her homegirls. Does she stay home or go out?")
+                        Text("   Amanda stared at her reflection in the mirror feeling down from her day. She lost a huge case for the firm. She wondered if she should go out or stay home. Just then she received a text from her homegirls. Does she stay home or go out?")
                         
                         Toggle(isOn: $goOut) {
                             Text(goOut == false ? "Stay home" : "Go out")
                                 .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                        }.padding(.bottom, 40)
+                        }
+//                        .padding(.bottom, 40)
                         
                     }
                     
-                    VStack {
-                        Picker("Friends", selection: $friend) {ForEach(friends, id: \.self) { index in Text(index)}
-                        }
-                        .pickerStyle(SegmentedPickerStyle())
-                        .fontWeight(.bold)
-                        
-                        Picker("Drinks", selection: $drink) {ForEach(drinks, id: \.self) { index in Text(index)}
-                        }
-                        .pickerStyle(InlinePickerStyle())
-                        .fontWeight(.heavy)
-
-                        
-                        Stepper(value: $shots, in: 0...10, step: 1) {Text("Number of Shots: \(shots)")
+                    
+                        VStack {
+                            Picker("Friends", selection: $friend) {ForEach(friends, id: \.self) { index in Text(index)}
                             }
-                        TextField("How should Amanda get home?", text: $rideHome)
+                            .pickerStyle(SegmentedPickerStyle())
                             .fontWeight(.bold)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding(.bottom)
+                            
+                            VStack {
+                                HStack {
+                                    Button("Party Time") {
+                                        print("Drink up ladies!")
+                                    }
+                                    Button("Slow Down") {
+                                        print("We had enough!")
+                                    }
+                                }
+                            
+                            Picker("Drinks", selection: $drink) {ForEach(drinks, id: \.self) { index in Text(index)}
+                            }
+                            .pickerStyle(InlinePickerStyle())
+                            .fontWeight(.heavy)
+                            
+                            
+                            Stepper(value: $shots, in: 0...10, step: 1) {Text("Number of Shots: \(shots)")
+                            }
+                            TextField("How should Amanda get home?", text: $rideHome)
+                                .fontWeight(.bold)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                           
+                                switch rideHome {
+                                case "Bus":
+                                    Text("She fell asleep and missed her stop.")
+                                case "Friend":
+                                    Text("The weird guy is always available.")
+                                case "Uber":
+                                    Text("Threw up in the backseat, byebye 5 star rating")
+                                case "Truck":
+                                    Text("Police pulled her over")
+                                case "Walk":
+                                    Text("Broke her heel and hobbled home")
+                                default:
+                                    Text("")
+                                }
+//                                .padding()
+//                            Spacer()
+                        }
                         
-                        .padding()
-                        Spacer()
                     }
-                    
-                }
+                }.padding()
                 .navigationTitle("Things go Wrong")
             }
         }
